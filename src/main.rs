79234584696cfec0c0c1b26a6c80e94769bbbebe
LordOsslor <oskar.weymann@student.hu-berlin.de>
2023-12-02 01:51:07 +0100

    Remove clippy config

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,3 @@
-#![warn(
-    clippy::all,
-    clippy::restriction,
-    clippy::pedantic,
-    clippy::nursery,
-    clippy::cargo
-)]
-
 use clap::Parser;
 use core::panic;
 use simple_logger::SimpleLogger;
